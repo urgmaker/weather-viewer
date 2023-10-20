7624d0b6d283f5d4f9049d15e5e893e83e43f8e2
urgmaker <uranovrg@gmail.com>
2023-10-20 12:38:25 +0300

    Add User

--- src/main/java/pet/project/models/User.java
+++ src/main/java/pet/project/models/User.java
@@ -1,4 +1,28 @@
 package pet.project.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @SequenceGenerator(name = "users_seq", sequenceName = "users_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+    private Long id;
+
+    @Column(unique = true)
+    private String login;
+
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
 }
