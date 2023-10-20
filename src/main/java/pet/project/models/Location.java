f32394e1f4e361ce5482ef7b15a2b50627d8c57e
urgmaker <uranovrg@gmail.com>
2023-10-20 12:37:59 +0300

    Add Location and Session

--- src/main/java/pet/project/models/Location.java
+++ src/main/java/pet/project/models/Location.java
@@ -1,4 +1,37 @@
 package pet.project.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "locations", uniqueConstraints = {@UniqueConstraint(columnNames = {"latitude", "longitude"})})
 public class Location {
+    @Id
+    @SequenceGenerator(name = "locations_seq", sequenceName = "locations_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "locations_seq")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinColumn(name = "user_id")
+    private List<User> users;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    public Location(String name, List<User> users, Double latitude, Double longitude) {
+        this.name = name;
+        this.users = users;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
--- src/main/java/pet/project/models/Session.java
+++ src/main/java/pet/project/models/Session.java
@@ -1,4 +1,31 @@
 package pet.project.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "sessions")
 public class Session {
+    @Id
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+
+    public Session(User user, LocalDateTime expiresAt) {
+        this.user = user;
+        this.expiresAt = expiresAt;
+    }
 }
