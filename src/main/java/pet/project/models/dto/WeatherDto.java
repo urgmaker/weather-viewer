3381b69ea9cc1c6d480fe6cebb78597b1a389b32
urgmaker <uranovrg@gmail.com>
2023-10-20 12:36:31 +0300

    Add WeatherDto

--- src/main/java/pet/project/models/dto/WeatherDto.java
+++ src/main/java/pet/project/models/dto/WeatherDto.java
@@ -1,4 +1,46 @@
 package pet.project.models.dto;
 
+import lombok.*;
+import pet.project.models.dto.enums.TimeOfDay;
+import pet.project.models.dto.enums.WeatherCondition;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class WeatherDto {
+    private WeatherCondition weatherCondition;
+
+    private TimeOfDay timeOfDay;
+
+    private String description;
+
+    private Double temperature;
+
+    private Double temperatureFeelsLike;
+
+    private Double temperatureMinimum;
+
+    private Double temperatureMaximum;
+
+    private Integer humidity;
+
+    private Integer pressure;
+
+    private Double windSpeed;
+
+    private Integer windDirection;
+
+    private Double windGust;
+
+    private Integer cloudiness;
+
+    private Date date;
+
+    private Date sunrise;
+
+    private Date sunset;
 }
